6d9f60d236ba051e37541cb96bde3a48e66f8318
Roger Garcia <garcciaroger@gmail.com>
2023-04-01 02:08:39 -0500

    edited

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
 
     //Ask user to enter information
     int numExam, numStudents;
-    double grades, averageGrade,sumGrade;
+    double averageGrade, sumGrade;
     cout << "Enter the number of exams " << endl;
     cin >> numExam;
     cout << "Enter the number of students " << endl;
@@ -20,23 +20,28 @@ int main() {
 
     //This block of code will allow the user to start entering the number of
     //grades and number of students
-    for(int i{0};i<numStudents;++i){
-        cout << "Enter Grades for student: " << i+1 << endl;
-        for(int j{0};j<numExam;++j){
+    for (int i{0}; i < numStudents; ++i) {
+        cout << "Enter Grades for student: " << i + 1 << endl;
+        for (int j{0}; j < numExam; ++j) {
+
+            vector<double> examGrade{};
             //User enters the exam grade
-            cout << "Enter Exam " << (j+1) << endl;
+            cout << "Enter Exam " << (j + 1) << endl;
             cin >> grades;
+
+
             //Validates grade entry
-            if(grades < 0 || grades > 100) {
+            if (grades < 0 || grades > 100) {
                 cout << "Please enter a valid grade." << endl;
                 cin >> grades;
             }
             //Calculate the grade average
-            sumGrade = grades+=grades;
-            averageGrade = sumGrade/grades;
+            sumGrade = grades += grades;
+            averageGrade = sumGrade / grades;
         }
         cout << "The Average Grade for student One is " << averageGrade << endl;
     }
+}
 
 
 
