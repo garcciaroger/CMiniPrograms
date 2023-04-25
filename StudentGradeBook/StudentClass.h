900d9d4c4cca3414816f6608e293c0733f972e40
garcciaroger <102135530+garcciaroger@users.noreply.github.com>
2023-04-25 18:36:20 -0500

    Update StudentClass.h

--- StudentGradeBook/StudentClass.h
+++ StudentGradeBook/StudentClass.h
@@ -1,6 +1,5 @@
 #ifndef STUDENTGRADEBOOK_STUDENTCLASS_H
 #define STUDENTGRADEBOOK_STUDENTCLASS_H
-
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -11,17 +10,25 @@ class Student{
 private:
     string Name;
     int StudentID;
+    double average;
     vector<double> Grades;
 public:
-    Student(string name, int ID, vector<double>&Grades)
+    Student(string name, int ID)
     {
        Name = name;
        StudentID = ID;
     }
-    void getGrades(vector<double> &Grades){
+    string getName() const {
+        return Name;
+    }
+    void getGrades(){
         int totalOfGrades;
         cout << "How many grades are you going to enter?" << endl;
         cin >> totalOfGrades;
+        while(totalOfGrades < 0){
+            cout << "Please enter valid number of grades" << endl;
+            cin >> totalOfGrades;
+        }
         for(int i = 0; i < totalOfGrades; i++){
             double grades;
             cout << "Enter Exam Grade: ";
@@ -30,20 +37,40 @@ public:
         }
         cout << endl;
     }
-    void displayGrades(vector<double>&Grades, string name, int StudentID    ){
-        cout << "Name: " << name << endl;
+    double averageGrade(){
+        double sum = 0;
+        for(int i = 0; i < Grades.size(); i++){
+            sum += Grades[i];
+        }
+        average = Grades.empty() ? 0.0 : sum / Grades.size();
+        return average;
+    }
+    double calculateAverage(){
+        double sum = 0;
+        for(int i = 0; i < Grades.size(); i++){
+            sum += Grades[i];
+        }
+        average = Grades.empty() ? 0.0 : sum / Grades.size();
+        cout << "Average Grade: " << average << endl;
+        return average;
+    }
+    void displayGrades(){
+        cout << "Name: " << Name << endl;
         cout << "ID: " << StudentID << endl;
         for(int i = 0; i < Grades.size(); i++){
             cout << "Grade " << i + 1  << " - " << Grades[i] << endl;
         }
     }
-    void calculateGrades(vector<double> &Grades){
-        double sum = 0;
-        for(int i = 0; i < Grades.size(); i++){
-           sum += Grades[i];
+    void calculateLetterGrade(){
+        if(average >=90){
+            cout << "You earned an A!" << endl;
+        }else if(average <= 89 && average >= 80){
+            cout << "You earned a B!" << endl;
+        }else if(average <= 79 && average >=70){
+            cout << "You earned a C!" << endl;
+        }else if(average <=69){
+            cout << "You failed dumbass!" << endl;
         }
-        double average = sum/Grades.size();
-        cout << "The average grade for is: " << average << "%" << endl;
     }
 };
 #endif //STUDENTGRADEBOOK_STUDENTCLASS_H
